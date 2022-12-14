b607a9436124f2e8d0dd94cfc2aa02b5f53fdbdb
James Mount <jmount1992@gmail.com>
2022-12-14 15:26:52 -0800

    Initial file IO functionality created.

---
+++ robotools/file_utils.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+
+##################################################################################
+# MIT License                                                                    #
+#                                                                                #
+# Copyright (c) 2022 James Mount                                                 #
+#                                                                                #
+# Permission is hereby granted, free of charge, to any person obtaining a copy   #
+# of this software and associated documentation files (the "Software"), to deal  #
+# in the Software without restriction, including without limitation the rights   #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      #
+# copies of the Software, and to permit persons to whom the Software is          #
+# furnished to do so, subject to the following conditions:                       #
+#                                                                                #
+# The above copyright notice and this permission notice shall be included in all #
+# copies or substantial portions of the Software.                                #
+#                                                                                #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #
+# SOFTWARE.                                                                      #
+##################################################################################
+
+###############
+### MODULES ###
+###############
+
+import pathlib
+from typing import List, Union
+
+import numpy as np
+
+import cv2
+from PIL import Image
+import open3d as o3d
+
+from robotools.defines import FrameType, ImageFormat
+
+
+########################
+### PUBLIC FUNCTIONS ###
+########################
+
+def supported_image_types() -> List:
+    """Get the list of supported image extensions. The period is not included.
+
+    Returns:
+        List: The list of supported image types.
+    """
+    return ['bmp', 'pbm', 'pgm', 'ppm', 'jpeg', 'jpg', 'jpe', 'jp2', 'tiff', 'tif', 'png']
+
+
+def supported_pointcloud_types() -> List:
+    """Get the list of supported point cloud extensions. The period is not included.
+
+    Returns:
+        List: The list of supported point cloud types.
+    """
+    return ['ply', 'pcd']
+
+
+def supported_csv_types() -> List:
+    """Get the list of supported CSV file extensions. The period is not included.
+
+    Returns:
+        List: The list of supported CSV file types.
+    """
+    return ['csv']
+
+
+def extension_from_filepath(filepath: pathlib.Path) -> str:
+    """Gets the extension for a file given its filepath. The filepath can be relative or absolute.
+
+    Args:
+        filepath (pathlib.Path): The relative or absolute path to the file.
+
+    Returns:
+        str: The extension for the file without the period.
+    """
+    filepath = pathlib.Path(filepath)
+    if len((filepath.name).rsplit('.',1)) == 2:
+        return (filepath.name).rsplit('.',1)[1]
+    return None
+
+
+def frametype_from_extension(extension: str) -> FrameType:
+    """Returns the FrameType given the file extension.
+
+    Args:
+        extension (str): The file extension without the period.
+
+    Returns:
+        FrameType: The FrameType for the given extension.
+    """
+
+    if extension in supported_image_types():
+        return FrameType.IMAGE
+    elif extension in supported_pointcloud_types():
+        return FrameType.POINTCLOUD
+    elif extension in supported_csv_types():
+        return FrameType.CSVDATA
+    
+    return FrameType.UNKNOWN
+
+
+def frametype_from_filepath(filepath: pathlib.Path) -> FrameType:
+    """Returns the FrameType given the filepath.
+
+    Args:
+        filepath (pathlib.Path): The relative or absolute path to the file.
+
+    Returns:
+        FrameType: The FrameType for the given filepath.
+    """
+    extension = extension_from_filepath(filepath)
+    return frametype_from_extension(extension)
+
+
+def read_image(filepath: pathlib.Path, image_format: ImageFormat = ImageFormat.OPENCV, colour: bool=True) -> Union[np.ndarray, Image.Image]:
+    """Reads an image either as an OpenCV image (Numpy array, default behaviour) or as a PIL Image.
+
+    Args:
+        filepath (pathlib.Path): The file path to the image to be read.
+        image_format (ImageFormat, optional): The format to use. Options are ImageFormat.OPENCV or ImageFormat.PIL. Defaults to OpenCV.
+        colour (bool, optional): Set to False to read the image as grayscale. Defaults to True. 
+    """   
+
+    # Check if auto-detect if colour or grayscale is on, or user wants specific format
+    if image_format == ImageFormat.OPENCV:
+        return read_image_opencv(filepath, colour)
+    elif image_format == ImageFormat.PIL:
+        return read_image_pil(filepath, colour)
+
+    # Raise value error
+    raise ValueError("Unknown image format %d."%(image_format))
+    
+
+def read_pointcloud(filepath: pathlib.Path) -> o3d.geometry.PointCloud:
+    """Reads a point cloud and returns an Open3d Geometry PointCloud object.
+
+    Args:
+        filepath (pathlib.Path): The file path to the point cloud to be read.
+
+    Returns:
+        o3d.geometry.PointCloud: The read point cloud.
+    """
+
+    pcd = o3d.io.read_point_cloud(str(filepath))
+    return pcd
+
+#########################
+### PRIVATE FUNCTIONS ###
+#########################
+
+
+def read_image_opencv(filepath: pathlib.Path, colour: bool = True) -> np.ndarray:
+    """Reads an image as an OpenCV image (Numpy array).
+
+    Args:
+        filepath (pathlib.Path): The file path to the image to be read.
+        colour (bool, optional): Set to False to read the image as grayscale. Defaults to True.
+
+    Returns:
+        np.ndarray: The read image.
+    """
+
+    # Check if auto-detect if colour or grayscale is on, or user wants specific format
+    if colour != None:
+        if colour:
+            img = cv2.imread(str(filepath), cv2.IMREAD_COLOR)
+        else:
+            img = cv2.imread(str(filepath), cv2.IMREAD_GRAYSCALE)
+    else:
+        img = cv2.imread(str(filepath), cv2.IMREAD_COLOR)
+        b,g,r = img[:,:,0], img[:,:,1], img[:,:,2]
+        if (b==g).all() and (b==r).all():
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Return
+    return img
+
+
+def read_image_pil(filepath: pathlib.Path, colour: bool = True) -> Image.Image:
+    """Reads an image into a PIL.Image.Image object. 
+
+    Args:
+        filepath (pathlib.Path): The file path to the image to be read.
+        colour (bool): Set to False to read the image as grayscale. Defaults to True.
+
+    Returns:
+        Image.Image: The read image.
+    """
+    
+    # PIL appears to automatically choose the best "mode" (grayscale, RGB, etc.)
+    img = Image.open(str(filepath))
+
+    # Force to grayscale
+    if colour == False and img.mode != "L":
+        img = img.convert('L')
+
+    # Return
+    return img
---
+++ tests/test_file_utils.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+##################################################################################
+# MIT License                                                                    #
+#                                                                                #
+# Copyright (c) 2022 James Mount                                                 #
+#                                                                                #
+# Permission is hereby granted, free of charge, to any person obtaining a copy   #
+# of this software and associated documentation files (the "Software"), to deal  #
+# in the Software without restriction, including without limitation the rights   #
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      #
+# copies of the Software, and to permit persons to whom the Software is          #
+# furnished to do so, subject to the following conditions:                       #
+#                                                                                #
+# The above copyright notice and this permission notice shall be included in all #
+# copies or substantial portions of the Software.                                #
+#                                                                                #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #
+# SOFTWARE.                                                                      #
+##################################################################################
+
+###############
+### MODULES ###
+###############
+
+import os
+import cv2
+import pathlib
+import numpy as np
+from PIL import Image, ImageChops
+import open3d as o3d
+
+from robotools.defines import FrameType
+from robotools.file_utils import *
+
+SCRIPT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+
+################################
+### FILE UTIL FUNCTION TESTS ###
+################################
+
+# Testing extension_from_filepath function
+# Returns correct extension with an absolute path
+def test_extension_from_filepath_absolute():
+    filepath = "/path/to/file/001.png"
+    assert extension_from_filepath(filepath) == "png"
+
+# Returns correct extension with relative paths
+def test_extension_from_filepath_relative():
+    filepath = "001.png"
+    assert extension_from_filepath(filepath) == "png"
+
+    filepath = "relative/001.png"
+    assert extension_from_filepath(filepath) == "png"
+
+
+# Testing frametype_from_extension function
+# Returns correct frame type for image extensions
+def test_frametype_from_extension_image_types():
+    for _type in supported_image_types():
+        assert frametype_from_extension(_type) == FrameType.IMAGE
+
+# Returns correct frame type for point cloud extensions
+def test_frametype_from_extension_pointcloud_types():
+    for _type in supported_pointcloud_types():
+        assert frametype_from_extension(_type) == FrameType.POINTCLOUD
+
+# Returns correct frame type for csv extensions
+def test_frametype_from_extension_csv_types():
+    for _type in supported_csv_types():
+        assert frametype_from_extension(_type) == FrameType.CSVDATA
+
+# Returns correct frame type for unknown extensions
+def test_frametype_from_extension_unknown_types():
+    assert frametype_from_extension("some-extension") == FrameType.UNKNOWN
+
+
+# Testing frametype_from_filepath function
+# Returns correct frame type for absolute and relative file paths
+def test_frametype_from_filepath():
+    filepath = "/path/to/file/001.png"
+    assert frametype_from_filepath(filepath) == FrameType.IMAGE
+
+    filepath = "001.png"
+    assert frametype_from_filepath(filepath) == FrameType.IMAGE
+
+    filepath = "relative/001.png"
+    assert frametype_from_filepath(filepath) == FrameType.IMAGE
+
+
+# Testing read_image using OpenCV format
+def test_read_image_opencv_colour():
+    filepath = SCRIPT_DIR / "data/starry_night.jpg"
+    img1 = read_image(filepath)
+    img2 = cv2.imread(str(filepath), cv2.IMREAD_COLOR)
+    assert (img1.shape == img2.shape and not(np.bitwise_xor(img1,img2).any())) == True
+
+def test_read_image_opencv_grayscale():
+    filepath = SCRIPT_DIR / "data/starry_night.jpg"
+    img1 = read_image(filepath, colour=False)
+    img2 = cv2.imread(str(filepath), cv2.IMREAD_GRAYSCALE)
+    assert (img1.shape == img2.shape and not(np.bitwise_xor(img1,img2).any())) == True
+
+def test_read_image_opencv_auto_colour():
+    filepath = SCRIPT_DIR / "data/starry_night.jpg"
+    img1 = read_image(filepath, colour=None)
+    img2 = cv2.imread(str(filepath), cv2.IMREAD_COLOR)
+    assert (img1.shape == img2.shape and not(np.bitwise_xor(img1,img2).any())) == True
+
+def test_read_image_opencv_auto_grayscale():
+    filepath = SCRIPT_DIR / "data/starry_night_gray.jpg"
+    img1 = read_image(filepath, colour=None)
+    img2 = cv2.imread(str(filepath), cv2.IMREAD_GRAYSCALE)
+    assert (img1.shape == img2.shape and not(np.bitwise_xor(img1,img2).any())) == True
+
+# Testing read_image using PIL format
+def test_read_image_pil_colour():
+    filepath = SCRIPT_DIR / "data/starry_night.jpg"
+    img1 = read_image(filepath, ImageFormat.PIL)
+    img2 = Image.open(str(filepath))
+    assert (ImageChops.difference(img1, img2)).getbbox() == None
+
+def test_read_image_pil_grayscale():
+    filepath = SCRIPT_DIR / "data/starry_night.jpg"
+    img1 = read_image(filepath, ImageFormat.PIL, colour=False)
+    img2 = Image.open(str(filepath)).convert("L")
+    assert (ImageChops.difference(img1, img2)).getbbox() == None
+
+def test_read_image_pil_auto_colour():
+    filepath = SCRIPT_DIR / "data/starry_night.jpg"
+    img1 = read_image(filepath, ImageFormat.PIL, colour=None)
+    img2 = Image.open(str(filepath))
+    assert (ImageChops.difference(img1, img2)).getbbox() == None
+
+def test_read_image_pil_auto_grayscale():
+    filepath = SCRIPT_DIR / "data/starry_night_gray.jpg"
+    img1 = read_image(filepath, ImageFormat.PIL, colour=None)
+    img2 = Image.open(str(filepath))
+    assert (ImageChops.difference(img1, img2)).getbbox() == None
+
+# Testing read_pointcloud
+def test_read_pointcloud():
+    filepath = SCRIPT_DIR / "data/fragment.ply"
+    pcd1 = read_pointcloud(filepath)
+    pcd2 = o3d.io.read_point_cloud(str(filepath))
+
+    pcd1_pts = np.asarray(pcd1.points)
+    pcd1_col = np.asarray(pcd1.colors)
+    pcd2_pts = np.asarray(pcd2.points)
+    pcd2_col = np.asarray(pcd2.colors)
+    assert np.sum(pcd1_pts - pcd2_pts) == 0
+    assert np.sum(pcd1_col - pcd2_col) == 0
+    
+
+
